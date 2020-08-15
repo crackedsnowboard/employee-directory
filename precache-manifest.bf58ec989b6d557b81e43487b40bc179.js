9e51dfa27a39009792664d295df0e81fffb50e7c
Prince of Kochi <mathenjs@gmail.com>
2020-08-14 19:53:23 -0700

    Updates

---
+++ precache-manifest.bf58ec989b6d557b81e43487b40bc179.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3daf2acf0d11f519a932e1f3139853b",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "dab134941957db8697ef",
+    "url": "/employee-directory/static/css/main.720c836c.chunk.css"
+  },
+  {
+    "revision": "838781224de8bfdc7c6b",
+    "url": "/employee-directory/static/js/2.6929d698.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee-directory/static/js/2.6929d698.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dab134941957db8697ef",
+    "url": "/employee-directory/static/js/main.52ec209f.chunk.js"
+  },
+  {
+    "revision": "f15086566d69cacd5722",
+    "url": "/employee-directory/static/js/runtime-main.917b9920.js"
+  }
+]);
